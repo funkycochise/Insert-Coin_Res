942d7a29ea442262698861bad64fe36a037500a2
funkycochise <funkycochise@free.fr>
2021-10-30 12:29:48 +0200

    Add files via upload

--- install_coinop.sh
+++ install_coinop.sh
@@ -4,7 +4,7 @@ ZP="coinop.zip"
 
 cd /media/fat/scripts
 rm -r coinop >/dev/null
-rm $ZP  >/dev/null
+rm $ZP >/dev/null
 
 curl https://raw.githubusercontent.com/funkycochise/Coin-Op/master/$ZP -O -k
 unzip $ZP -d /media/fat/scripts/
