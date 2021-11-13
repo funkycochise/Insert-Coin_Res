506c65ed78a8f8822e168ccd48c1cf6641c7e1ac
funkycochise <funkycochise@free.fr>
2021-11-13 11:42:21 +0100

    Add files via upload

---
+++ symlink_neogeo.sh
@@ -0,0 +1,11 @@
+SrcDir=/media/fat/games/NEOGEO
+OutDir=/media/fat/games/mame
+
+#echo "$SrcDir";
+
+for completefile in $SrcDir/*.zip;
+do
+   file=${completefile##*/}
+   echo "$SrcDir/$file -> $OutDir/$file";
+   ln -s "$SrcDir/$file" "$OutDir/$file";
+done
