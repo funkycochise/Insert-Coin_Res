42ca2ab27fb61e4a031ab5e637422069048811dc
funkycochise <funkycochise@free.fr>
2022-01-05 06:59:48 +0100

    remove coinop sh

---
+++ remove_coinop.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Removing Coin-Op Folder (leaving all additional cores and mras)"
+rm -r "/media/fat/scripts/coinop">/dev/null
+rm -r "/media/fat/_Arcade/_Coin-Op">/dev/null
+echo "Done"
+
+
