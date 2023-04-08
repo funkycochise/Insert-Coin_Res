8cc2554055462992568b8459fe591a9c5a1ace1f
funkycochise <funkycochise@free.fr>
2023-04-08 08:33:27 +0200

    echo

--- clean_console.sh
+++ clean_console.sh
@@ -1,16 +1,10 @@
 # redirect stdout/stderr to a file
 #exec >run.txt 2>&1
 
-exec 3>&1
-special_echo () {
-    echo "$@" >&3
-}
-exec &>/dev/null
-
 launchdir=$1
 #special_echo "launchdir $launchdir"
 
-special_echo "Cleaning console mgl"
+echo "Cleaning console mgl"
 
 #gameboy color ? remove
 if [ -f "/media/fat/_Console/GameboyColor.mgl" ] 
@@ -23,4 +17,4 @@ then
    rm -r "/media/fat/_Console/WonderSwan Color.mgl"
 fi
 
-special_echo "Completed."
+echo "Completed."
