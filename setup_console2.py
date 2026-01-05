83cf9e97479c04f3fe3855bf2589ee91cf74a2dd
funkycochise <funkycochise@free.fr>
2026-01-05 10:24:12 +0100

    Add files via upload

--- setup_console2.py
+++ setup_console2.py
@@ -7,6 +7,7 @@ import subprocess
 INI_FILE = "setup.ini"
 IGNORE_SECTIONS = ["reserved", "setup"]
 
+
 # --- Valeurs par défaut ---
 DEFAULT_CONFIG = {
     "update": {
@@ -56,13 +57,145 @@ DEFAULT_CONFIG = {
     }
 }
 
-# --- Création du fichier INI si inexistant ---
-if not os.path.exists(INI_FILE):
+NAMES_INI_FILE = "names.ini"
+
+# Valeurs par défaut pour names.ini
+NAMES_DEFAULT_CONFIG = {
+    "folder": {
+        "essential": "_#Essentials",
+        "newest": "_#Newest",
+        "genre_horizontal": "__Horizontal",
+        "genre_vertical": "__Vertical",
+        "insertcoin": "_#Insert-Coin",
+        "genre_action": "__Action",
+        "genre_beat": "__Beat'em up",
+        "genre_puzzle": "__Puzzle",
+        "genre_sport": "__Sport",
+        "genre_vsf": "__Vs Fighting",
+        "genre_stg_h": "__STG_H",
+        "genre_stg_v": "__STG_V",
+        "genre_rng_h": "__Run'n'Gun_H",
+        "genre_rng_v": "__Run'n'Gun_V",
+        "alpha": "_Alpha",
+        "atari": "_Atari",
+        "bagman": "_Bagman",
+        "bally_midway": "_Bally-midway",
+        "capcom": "_Capcom-Mitchell",
+        "cps1": "_CPS1",
+        "cps15": "_CPS15",
+        "cps2": "_CPS2",
+        "cave": "_Cave 68000",
+        "crazykong": "_Crazy Kong",
+        "deco": "_DataEast-Deco",
+        "exidy": "_Exidy",
+        "galaxian": "_Galaxian",
+        "gottlieb": "_Gottlieb",
+        "irem": "_Irem",
+        "irem62": "_Irem M62",
+        "irem72": "_Irem M72",
+        "irem90": "_Irem M90",
+        "irem92": "_Irem M92",
+        "irem92t": "_Irem M92t",
+        "irem107": "_Irem M107",
+        "jaleco": "_Jaleco",
+        "kiwako": "_Kiwako",
+        "konami": "_Konami",
+        "konamitwin16": "_Konami Twin16",
+        "ladybug": "_Ladybug",
+        "mcr1": "_MCR1",
+        "mcr2": "_MCR2",
+        "mcr3": "_MCR3",
+        "mcr3mono": "_Midway_MCR3Mono",
+        "mcr3scroll": "_Midway_MCR3Scroll",
+        "midwayy": "_Midway_YUnit",
+        "namco": "_Namco",
+        "namco_sys1": "_Namco-System-1",
+        "namco_sys86": "_Namco-System-86",
+        "neogeo": "_Neo-geo",
+        "nichibutsu": "_Nihon Bussan-Nichibutsu",
+        "nintendo": "_Nintendo",
+        "vs": "_Nintendo Vs.",
+        "pacman": "_Pacman",
+        "raizing": "_Raizing-8ing",
+        "rare": "_Rare",
+        "robotron": "_Robotron",
+        "scramble": "_Scramble",
+        "sega": "_Sega",
+        "outrun": "_Sega-Outrun",
+        "segasys1": "_Sega-System-1",
+        "segasys2": "_Sega-System-2",
+        "segasyse": "_Sega-System-E",
+        "segasys16": "_Sega-System-16",
+        "segasys18": "_Sega-System-18",
+        "segastv": "_Sega-Titan Video",
+        "snk": "_SNK",
+        "si": "_Space Invaders",
+        "stern": "_Stern",
+        "tad": "_Tad Corp",
+        "taito": "_Taito",
+        "taitof2": "_Taito-F2",
+        "taitosj": "_Taito-SJ",
+        "technos": "_Technos",
+        "technos16": "_Technos16",
+        "tecmo": "_Tehkan-Tecmo",
+        "toaplan": "_Toaplan",
+        "toaplan_stg": "_Toaplan_STG",
+        "universal": "_Universal",
+        "upl": "_Upl",
+        "williams": "_Williams"
+    }
+}
+
+# --- Fonction pour créer / compléter setup.ini ---
+def ensure_setup_ini():
     parser = configparser.ConfigParser()
+    updated = False
+
+    # Lire le fichier existant
+    if os.path.exists(INI_FILE):
+        parser.read(INI_FILE, encoding="utf-8")
+
+    # Ajouter / compléter les sections et clés par défaut
     for sec, opts in DEFAULT_CONFIG.items():
-        parser[sec] = opts
-    with open(INI_FILE, "w", encoding="utf-8") as f:
-        parser.write(f)
+        if sec not in parser:
+            parser[sec] = {}
+            updated = True
+        for key, val in opts.items():
+            if key not in parser[sec]:
+                parser[sec][key] = val
+                updated = True
+
+    # Réécrire si nécessaire
+    if updated or not os.path.exists(INI_FILE):
+        with open(INI_FILE, "w", encoding="utf-8") as f:
+            parser.write(f)
+            
+# --- Fonction pour créer / compléter names.ini ---
+def ensure_names_ini():
+    parser = configparser.ConfigParser()
+    updated = False
+
+    # Lire le fichier existant
+    if os.path.exists(NAMES_INI_FILE):
+        parser.read(NAMES_INI_FILE, encoding="utf-8")
+
+    # Ajouter / compléter les clés par défaut
+    for sec, opts in NAMES_DEFAULT_CONFIG.items():
+        if sec not in parser:
+            parser[sec] = {}
+            updated = True
+        for key, val in opts.items():
+            if key not in parser[sec]:
+                parser[sec][key] = val
+                updated = True
+
+    # Réécrire si nécessaire
+    if updated or not os.path.exists(NAMES_INI_FILE):
+        with open(NAMES_INI_FILE, "w", encoding="utf-8") as f:
+            parser.write(f)
+
+ensure_setup_ini()
+ensure_names_ini()
 
 # --- Lecture INI et copie en mémoire ---
 parser = configparser.ConfigParser()
@@ -152,10 +285,17 @@ def cycle_dualsdram(sec, key):
     next_val = {"0": "1", "1": "2", "2": "0"}.get(val, "0")
     config[sec][key] = next_val
 
+# --- Sauvegarde avec section reserved ---
 def save_config():
     parser = configparser.ConfigParser()
+    
+    # Ajout de reserved en premier
+    parser["reserved"] = {"version": RESERVED_VERSION}
+    
+    # Ajout des autres sections
     for sec, opts in config.items():
         parser[sec] = opts
+    
     with open(INI_FILE, "w", encoding="utf-8") as f:
         parser.write(f)
 
@@ -187,14 +327,13 @@ def run_setup_menu(stdscr):
     current_section = 0
     current_key = 0
     mode = "section"
-
     main_menu = sections
 
     while True:
         stdscr.clear()
         stdscr.addstr(0, 0, "↑/↓ to browse, Enter/Space to toggle, Escape to go back", curses.color_pair(1))
 
-        # --- Affichage menu ---
+        # Affichage menu
         if mode == "section":
             stdscr.addstr(2, 0, "Select :", curses.color_pair(1))
             for i, sec in enumerate(main_menu):
@@ -223,11 +362,10 @@ def run_setup_menu(stdscr):
             else:
                 stdscr.addstr(3 + exit_index, 0, "  Exit", curses.color_pair(2))
 
-        # --- Affichage tooltip ---
+        # Tooltip
         tooltip = ""
         if mode == "section":
-            sec_name = main_menu[current_section]
-            tooltip = get_section_tooltip(sec_name)
+            tooltip = get_section_tooltip(main_menu[current_section])
         else:
             sec = main_menu[current_section]
             keys = list(config[sec].keys())
@@ -240,8 +378,8 @@ def run_setup_menu(stdscr):
         stdscr.refresh()
         key = stdscr.getch()
 
-        # --- Gestion des touches ---
-        if key == 27:  # Échap
+        # Gestion touches
+        if key == 27:
             if mode == "key":
                 mode = "section"
                 current_key = 0
@@ -250,18 +388,14 @@ def run_setup_menu(stdscr):
         elif key == curses.KEY_UP:
             if mode == "section":
                 current_section = (current_section - 1) % len(main_menu)
-            elif mode == "key":
-                sec = main_menu[current_section]
-                keys = list(config[sec].keys())
-                current_key = (current_key - 1) % (len(keys) + 1)
+            else:
+                current_key = (current_key - 1) % (len(config[main_menu[current_section]]) + 1)
         elif key == curses.KEY_DOWN:
             if mode == "section":
                 current_section = (current_section + 1) % len(main_menu)
-            elif mode == "key":
-                sec = main_menu[current_section]
-                keys = list(config[sec].keys())
-                current_key = (current_key + 1) % (len(keys) + 1)
-        elif key in [10, 13, 32]:  # Entrée ou Espace
+            else:
+                current_key = (current_key + 1) % (len(config[main_menu[current_section]]) + 1)
+        elif key in [10, 13, 32]:
             if mode == "section":
                 mode = "key"
                 current_key = 0
@@ -272,8 +406,7 @@ def run_setup_menu(stdscr):
                     mode = "section"
                     current_key = 0
                 else:
-                    k = keys[current_key]
-                    toggle_value(sec, k)
+                    toggle_value(sec, keys[current_key])
 
 # --- Menu principal ---
 def main(stdscr):
@@ -290,34 +423,31 @@ def main(stdscr):
         stdscr.clear()
         stdscr.addstr(0, 0, "↑/↓ to browse, Enter/Space to select, Escape to exit", curses.color_pair(1))
 
-        # Affichage menu
         for i, item in enumerate(main_menu):
             if i == current_selection:
                 stdscr.addstr(2 + i, 0, "> " + item, curses.color_pair(1) | curses.A_REVERSE)
             else:
                 stdscr.addstr(2 + i, 0, "  " + item, curses.color_pair(2))
 
-        # Tooltip
         tooltip = {
-            "Run": "Run Insert-Coin",
-            "Setup": "Insert-Coin configuration",
+            "Run": "Execute run.sh script",
+            "Setup": "Modify configuration",
             "Save": "Save current configuration",
             "Reset": "Reset configuration to defaults",
-            "Exit": "Exit"
+            "Exit": "Quit program"
         }.get(main_menu[current_selection], "")
         draw_tooltip(stdscr, tooltip)
 
         stdscr.refresh()
         key = stdscr.getch()
 
-        # Gestion touches
-        if key == 27:  # Échap
+        if key == 27:
             break
         elif key == curses.KEY_UP:
             current_selection = (current_selection - 1) % len(main_menu)
         elif key == curses.KEY_DOWN:
             current_selection = (current_selection + 1) % len(main_menu)
-        elif key in [10, 13, 32]:  # Entrée ou Espace
+        elif key in [10, 13, 32]:
             sel = main_menu[current_selection]
             if sel == "Exit":
                 break
